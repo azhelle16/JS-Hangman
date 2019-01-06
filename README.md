36ab76bfed3abce8726dc209d674bbab2747de40
mllsumulong <mllsumulong@gmail.com>
2019-01-05 21:49:30 -0800

    Added a selection for multiple categories Fixed bug in reset button where wins, loss and games played variables are not reset to 0 Added an exit button that brings back user to main page Added a button to change category while playing the game Updated the readme file
    Name    :   Maricel Louise Sumulong
    Date    :   January 05, 2019 PST
    E-mail  :   mllsumulong@gmail.com

--- README.md
+++ README.md
@@ -4,16 +4,23 @@ It's a basic game called Word Guess.
 
 In this game, the user will first see a greeting page asking for user's name. Once provided, user needs to press enter to go to the game.
 
-The category of the game is 90's movies.
+User can select from seven categories. The categories of the game are 90's movies, american tv shows, sports, music artists, scientists, fruits and animals.
 
 User can start guessing by pressing any keys. 
 
-User has 15 guesses to guess the name of the movie.
+User has 15 guesses to guess the word.
 
-If the user is able to guess the movie, the user will hear a chime and the word guessed will also be displayed for few seconds to see what the name of the movie was. The number of wins and games played will be updated.
+If the user is able to guess the word, the user will hear a chime and the word guessed will also be displayed for few seconds to see what the name of the movie was. The number of wins and games played will be updated.
 
-If the user used all 15 guesses before guessing the movie, the user will hear a buzzer and the correct word will also be displayed for few seconds so the user will see what the name of the movie was. The number of losses and games played will be updated.
+If the user used all 15 guesses before guessing the movie, the user will hear a buzzer and the correct word will also be displayed for few seconds so the user will see what the word was. The number of losses and games played will be updated.
 
 If user pressed a key that is already guessed, the user will also hear a sound.  
 
-If user clicks the reset button, the number of wins, losses and games played will be resetted to 0 and a new movie will be picked to guess.
+If user clicks the reset button, the number of wins, losses and games played will be resetted to 0 and a new word will be picked to guess.
+
+Special Credits:
+
+flamingtext.com - for the logo images
+clipartmax.com - for the rose logo
+wikipedia.com, imdb.com, rollingstone.com, billboard.com, goodhousekeeping.com - list of words to be guessed
+orangefreesounds.com - for the sounds used for this game
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,7 +21,8 @@ var gamePiece = {
 	totalGames : 0,
 	guessedLetters : [],
 	startGame : false,
-	wordBank : ["goodfellas","the game","jackie brown","boogie nights","the big lebowski","the player","the shawshank redemption","havana","stiff upper lips","ed wood", "forrest gump",
+	selectedCategory : "",
+	wordBank : {"movies":["goodfellas","the game","jackie brown","boogie nights","the big lebowski","the player","the shawshank redemption","havana","stiff upper lips","ed wood", "forrest gump",
 					"american beauty","being john malkovich","glengarry glen ross","the doors","magnolia","man on the moon","oscar","pretty woman","cape fear","the road to wellville","sleepers",
 					"titanic","true lies","the grifters","pulp fiction","heavenly creatures","leaving las vegas","under suspicion","fargo","trainspotting","casino","the matrix",
 					"great expectations","the remains of the day","absolute power","primal fear","the sixth sense","as good as it gets","guilty as sin","fight club","maverick","short cuts",
@@ -32,6 +33,48 @@ var gamePiece = {
 					"to die for","the truman show","dolores claiborne","falling down","reservoir dogs","amistad","natural born killers","in the name of the father","shining through",
 					"the inner circle","true romance","a perfect murder","lolita","air america","white hunter black heart","the client","intersection","disclosure","the mask of zorro",
 					"most wanted","sweet and lowdown","ronin","the trial","fanny and elvis","french kiss","the vanishing"],
+				"fruits":["melon","banana","honeydew","cantaloupe","watermelon","apple","acai","akee","apricot","avocado","bilberry","blackberry","blackcurrant","black sapote","blueberry","boysenberry",
+				    "crab apples","currant","cherry","custard apple","chico","cloudberry","coconut","cranberry","cucumber","damson","date","dragonfruit","durian","elderberry","feijoa","fig","goji berry",
+				    "gooseberry","grape","grapefruit","guava","honeyberry","huckleberry","jabuticaba","jackfruit","jambul","japanese plum","jostaberry","jujube","juniper berry","kiwano","kiwifruit",
+				    "kumquat","lemon","lime","loquat","longan","lychee","mango","mangosteen","marionberry","miracle fruit","mulberry","nectarine","nance","olive","blood orange","clementine","mandarine",
+				    "tangerine","papaya","passionfruit","peach","pear","persimmon","plantain","plum","pineapple","pineberry","plumcot","pomegranate","pomelo","quince","raspberry","rambutan","salak","satsuma",
+				    "soursop","star apple","star fruit","strawberry","tamarillo","tamarind","ugli fruit","yuzu"],
+				"atv":["the sopranos","the wire","breaking bad","mad men","seinfield","the simpsons","the twilight zone","saturday night live","all in the family","the daily show","freaks and geeks",
+				    "game of thrones","late night with david letterman","the larry sanders show","the west wing","m*a*s*h","twin peaks","star trek","curb your enthusiasm","cheers","the office","louie","deadwood",
+				    "friday night lights","veep","friends","arrested development","the x-files","sesame street","i love lucy","south park","my so-called life","orange is the new black","buffy the vampire slayer",
+				    "bones","american horror stories","lost","stranger things","pretty little liars","gossip girl","cougar town","the shield","the honeymooners","law & order","nypd blue","baywatch","the americans",
+				    "columbo","battlestar galactica","the rockford files","taxi","fargo","the colber report","the bob newhart show","the muppet show","six feet under","fawlty towers","roots","hill street blues",
+				    "beavis and butt-head","your show of shows","sex and the city","the wonder years","chapelle's show","happy days","downtown abbey","the odd couple","the state","the ed sullivan show",
+				    "roseanne","girls","golden girls","transparent","the ren & stimpy show","the walking dead","thirtysomething","in living color","the fugitive","dallas","the jeffersons",
+				    "house of cards","the real world","good times","doctor who","party down","homeland","the dick van dyke show","broad city","american idol","jeopardy","lizzie mcguire","that's so raven",
+				    "boy meets world","full house","desperate housewives","portlandia","the big bang theory","ghost whisperer","supernaturals","ncis","crime scene investigation","charmed","melrose place",
+				    "andi mack","girl meets world"],
+				"animals":["northern flicker","bowhead whale","rattlesnake","mockingbird","bear","lark bunting","sperm whale","gray fox","manatee","gopher tortoise","monk seal","appaloosa horse","white-tailed deer",
+				    "northern cardinal","american bullfrog","american bison","horse","alligator","moose","diamondback terrapin","seven-spotted ladybug","brook trout","common loon","wood duck","eastern bluebird",
+				    "mourning cloak","desert tortoise","desert bighorn sheep","red-spotted newt","european honey bee","roadrunner","beaver","eastern tiger swallowtail","northern black racer","eastern collared lizard",
+				    "dungeness crab","ruffed grouse","rhode island red","loggerhead sea turtle","coyote","raccoon","nine-banded armadillo","rocky mountain elk","monarch butterfly","virginia big-eared bat","orca",
+				    "black bear","badger","horned lizard"],
+				"artists":["the beatles","madonna","elton john","elvis presley","mariah carey","stevie wonder","janet jackson","michael jackson","whitney houston","rihanna","the rolling stones","bee gees","usher",
+					"chicago","the supremes","prince","daryl hall & john oates","rod stewart","olivia newton-john","drake","aretha franklin","marvin gaye","taylor swift","katy perry","phil collins","billy joel",
+					"diana ross","the four seasons","the temptations","donna summer","the beach boys","lionel richie","bruno mars","neil diamond","the carpenters","the jacksons","connie francis","beyonce","brenda lee",
+					"kenny rogers","barbra streisand","bryan adams","cher","george michael","the black eyed peas","pink","bobby vinton","john mellencamp","three dog night","huey lewis & the news","gloria estefan",
+					"bon jovi","chubby checker","ray charles","foreigner","chris brown","kool & the gang","gladys knight & the pips","ricky nelson","duran duran","justin timberlake","commodores","eagles","lady gaga",
+					"tlc","paul anka","barry manilow","dionne warwick","heart","nelly","the everly brothers","bobby darin","james brown","paula abdul","eminem","alicia keys","kelly clarkson","linda ronstadt","richard marx",
+					"starship","destiny's child","kanye west","celine dion","adele","jay-z","the miracles","nicki minaj","bob seger","fleetwood mac","ariana grande","neil sedaka","justin beiber","demi lovato","bruce springsteen",
+					"the pointer sisters","john denver","four tops","tony orlando & dawn","the weeknd","peabo bryson","lea salonga","miley cyrus","selena gomez","lea michele","queen","britney spears","christina aguilera",
+					"jessica simpson","toni braxton","rascal flatts","lucy hale","drew seeley","dua lipa","pentatonix","charlie puth"],
+				"scientists":["albert einstein","marie curie","isaac newton","galileo galilei","stephen hawking","charles darwin","nikola tesla","thomas edison","nicolaus copernicus","louis pasteur","michael faraday",
+					"alexander graham bell","francis crick","rosalind franklin","aristotle","james watson","neil degrasse tyson","leonardo da vinci","max planck","johannes kepler","niels bohr","antoine lavoisier",
+					"archimedes","gregor mendel","alexander fleming","james clerk maxwell","pierre curie","guglielmo marconi","richard feynman","edwin hubble","ernest rutherford","linus pauling","robert boyle","carl linnaeus",
+					"dmitri mendeleev","alessandro volta","heinrich hertz","erwin schroedinger","rachel carson","tim berners lee","paul dirac","wilhelm roentgen","tycho brahe","ada lovelace","enrico fermi","william herschel",
+					"robert hooke","andre-marie ampere","anaximander","luis alvarez","mary anning","aristarchus","amedeo avogadro","daniel bernoulli","robert bunsen","james chadwick","erwin chargaff","john dalton","rene descartes",
+					"erathosthenes","benjamin franklin","william harvey","joseph henry","grace hopper","jack honner","james hutton","irene joliot-curie","michio kaku","florence nightingale","alfred nobel","hans christian oersted",
+					"theodore schwann","thales of miletus","alfred wallace","james watt"],
+				"sports":["cheerleading","competitive dancing","dancesport","gymnastics","aerobatics","cluster ballooning","hopper ballooning","gliding","parachuting","paragliding","paramotoring","ultralight aviation",
+					"archery","badminton","biribol","bossaball","fistball","pickleball","sepak takraw","sipa","table tennis","tennis","throwball","volleyball","wallyball","basketball","cestoball","korfball","baseball",
+					"softball","cricket","skateboarding","scootering","skysurfing","streetluge","snowboarding","surfing","wakeboarding","paddleboarding","dodgeball","skiing","rock climbing","cycling","roller hockey","wrestling",
+					"judo","boxing","muay thai","taekwondo","fencing","eskrima","billiards","rodeo","jousting","horse polo","football","golf","soccer","swimming","rugby","figure skating","ice hockey","target shooting",
+					"raquetball","squash"]},
 	pickRandomWord : function() {
 
 		/*
@@ -41,17 +84,26 @@ var gamePiece = {
 		 #  AUTHOR        : Maricel Louise Sumulong
 		 #  DATE          : December 18, 2018 PST
 		 #  MODIFIED BY   : Maricel Louise Sumulong
-		 #  REVISION DATE : January 03, 2019 PST
-		 #  REVISION #    : 3
+		 #  REVISION DATE : January 05, 2019 PST
+		 #  REVISION #    : 4
 		 #  DESCRIPTION   : Selects random word in the list
-		 #  PARAMETERS    : none
+		 #  PARAMETERS    : category name
 		 #
  		 #######################################################################
 		*/
 
-		var index = Math.floor(Math.random() * ((gamePiece.wordBank.length - 1) - 0) + 0);
-		gamePiece.wordToGuess = gamePiece.wordBank[index];
-		gamePiece.wordSpace = gamePiece.wordToGuess.replace(/[a-z]/g,"-")
+		var categ = "";
+
+		switch (gamePiece.selectedCategory) {
+			case "90's Movies": categ = "movies"; break;
+			case "American TV Shows": categ = "atv"; break;
+			case "Music Artists": categ = "artists"; break;
+			default: categ = gamePiece.selectedCategory.toLowerCase();
+		}
+
+		var index = Math.floor(Math.random() * ((gamePiece.wordBank[categ].length - 1) - 0) + 0);
+		gamePiece.wordToGuess = gamePiece.wordBank[categ][index];
+		gamePiece.wordSpace = gamePiece.wordToGuess.replace(/[a-z]/g,"_")
 		document.getElementById("guessWordHere").innerHTML = gamePiece.wordSpace;
 
 	},
@@ -64,8 +116,8 @@ var gamePiece = {
 		 #  AUTHOR        : Maricel Louise Sumulong
 		 #  DATE          : December 18, 2018 PST
 		 #  MODIFIED BY   : Maricel Louise Sumulong
-		 #  REVISION DATE : January 03, 2019 PST
-		 #  REVISION #    : 7
+		 #  REVISION DATE : January 05, 2019 PST
+		 #  REVISION #    : 8
 		 #  DESCRIPTION   : Listens to user key inputs
 		 #  PARAMETERS    : key code event
 		 #
@@ -114,7 +166,7 @@ var gamePiece = {
 				final.innerHTML = gamePiece.wordToGuess.toUpperCase();
 				setTimeout(function(){
             		gamePiece.resetValues();
-            		final.style.color = "white";
+            		final.style.color = "#00ff83";
             		gamePiece.pickRandomWord();
             	}, 1500)
 				gamePiece.totalGames += 1;
@@ -138,17 +190,16 @@ var gamePiece = {
 		
 		} else if (event.keyCode == 13 && gamePiece.startGame == false) {
 				
-				var uname = document.getElementById("username").value;
 				document.getElementById("welcomeLogo").classList.add("dispHide");
-				document.getElementById("mainGame").classList.remove("dispHide");
+				document.getElementById("categories").classList.remove("dispHide");
 				gamePiece.startGame = true;
-				gamePiece.pickRandomWord();
-				document.getElementById("pname").innerHTML = uname;
-				var child = document.getElementById("welcomeAudio");
-				child.parentNode.removeChild(child);
-				document.getElementById("resetButton").onclick = function() {
-					gamePiece.reset();
-				}
+				var buttons = document.getElementsByClassName("buttonCat")
+				for (var x = 0; x < buttons.length; x++) {
+					buttons[x].onclick = function() {
+						gamePiece.selectedCategory = this.value;
+						gamePiece.setGameBoard();
+					}
+				}		
 
 	  	  } else {
 				return false
@@ -236,7 +287,7 @@ var gamePiece = {
 		 #  AUTHOR        : Maricel Louise Sumulong
 		 #  DATE          : December 18, 2018 PST
 		 #  MODIFIED BY   : Maricel Louise Sumulong
-		 #  REVISION DATE : December 25, 2018 PST
+		 #  REVISION DATE : January 05, 2019 PST
 		 #  REVISION #    : 2
 		 #  DESCRIPTION   : resets the whole game
 		 #  PARAMETERS    : none
@@ -253,7 +304,124 @@ var gamePiece = {
 		document.getElementById("gamesPlayed").innerHTML = 0;
 		gamePiece.pickRandomWord();
 		document.getElementById("gRem").style.color = "green";
+		gamePiece.totalLoss = 0;
+		gamePiece.totalWins = 0;
+		gamePiece.totalGames = 0;
 	
+	},
+	setGameBoard : function() {
+
+		/*
+ 		 #######################################################################
+		 #
+		 #  FUNCTION NAME : setGameBoard
+		 #  AUTHOR        : Maricel Louise Sumulong
+		 #  DATE          : January 05, 2018
+		 #  MODIFIED BY   : 
+		 #  REVISION DATE : 
+		 #  REVISION #    : 
+		 #  DESCRIPTION   : to set the game board with the selected category
+		 #  PARAMETERS    : none
+		 #
+		 #######################################################################
+		*/
+
+		document.getElementById("mainContainer").classList.remove("dispHide");
+		document.getElementById("categories").classList.add("dispHide");
+
+		var pics= "";
+
+		switch (gamePiece.selectedCategory) {
+			case "90's Movies": pics = "movies"; break;
+			case "American TV Shows": pics = "atv2"; break;
+			case "Music Artists": pics = "artists"; break;
+			default: pics = gamePiece.selectedCategory.toLowerCase();
+		}
+
+		document.getElementById("categPic").setAttribute("src","./assets/images/"+pics+".png")
+		gamePiece.pickRandomWord();
+		var uname = document.getElementById("username").value;
+		document.getElementById("pname").innerHTML = uname;
+		var child = document.getElementById("welcomeAudio");
+		child.parentNode.removeChild(child);
+		document.getElementById("resetButton").onclick = function() {
+			gamePiece.reset();
+		}
+		document.getElementById("changeCatButton").onclick = function() {
+			gamePiece.changeCatButton();
+		}
+		document.getElementById("exitButton").onclick = function() {
+			gamePiece.exitButton();
+		}
+
+	},
+	changeCatButton : function() {
+
+		/*
+ 		 #######################################################################
+		 #
+		 #  FUNCTION NAME : changeCatButton
+		 #  AUTHOR        : Maricel Louise Sumulong
+		 #  DATE          : January 05, 2018
+		 #  MODIFIED BY   : 
+		 #  REVISION DATE : 
+		 #  REVISION #    : 
+		 #  DESCRIPTION   : allows the user to change category
+		 #  PARAMETERS    : none
+		 #
+		 #######################################################################
+		*/
+
+		document.getElementById("guessLettersHere").innerHTML = ""
+		document.getElementById("gRem").innerHTML = 15;
+		gamePiece.guessedLetters = [];
+		document.getElementById("categories").classList.remove("dispHide");
+		document.getElementById("mainContainer").classList.add("dispHide");
+
+	},
+	exitButton : function() {
+
+		/*
+ 		 #######################################################################
+		 #
+		 #  FUNCTION NAME : exitButton
+		 #  AUTHOR        : Maricel Louise Sumulong
+		 #  DATE          : January 05, 2018
+		 #  MODIFIED BY   : 
+		 #  REVISION DATE : 
+		 #  REVISION #    : 
+		 #  DESCRIPTION   : goes back to the main page
+		 #  PARAMETERS    : none
+		 #
+		 #######################################################################
+		*/
+
+		gamePiece.wordSpace = "";
+		document.getElementById("guessLettersHere").innerHTML = ""
+		document.getElementById("gRem").innerHTML = 15;
+		gamePiece.guessedLetters = [];
+		document.getElementById("totalWins").innerHTML = 0;
+		document.getElementById("totalLoss").innerHTML = 0;
+		document.getElementById("gamesPlayed").innerHTML = 0;
+		document.getElementById("gRem").style.color = "green";
+		document.getElementById("categories").classList.add("dispHide");
+		document.getElementById("mainContainer").classList.add("dispHide");
+		document.getElementById("welcomeLogo").classList.remove("dispHide")
+		var toEmbed = document.createElement("embed")
+		document.querySelector('body').appendChild(toEmbed)
+		document.querySelector('embed').setAttribute("id","welcomeAudio");
+		document.querySelector('embed').setAttribute("src","./assets/images/sounds/electronic.mp3")
+		document.querySelector('embed').setAttribute("type","audio/mpeg")
+		document.querySelector('embed').setAttribute("autoplay","true")
+		document.querySelector('embed').setAttribute("hidden","true")
+		document.querySelector('embed').setAttribute("loop","true")
+		document.getElementById("username").value = "";
+		document.getElementById("username").focus();
+		gamePiece.startGame = false;
+		gamePiece.totalLoss = 0;
+		gamePiece.totalWins = 0;
+		gamePiece.totalGames = 0;
+
 	}
 
 }
